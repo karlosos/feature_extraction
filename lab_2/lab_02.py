5fcd69592246611922e9232bfe80352b9072f667
Karol Dzialowski <karlososhd@gmail.com>
2020-10-19 15:00:35 +0200

    fix: :bug: calculating base frequency in time domain

--- lab_2/lab_02.py
+++ lab_2/lab_02.py
@@ -6,25 +6,32 @@ import soundfile as sf
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 440 - 440
-# C3 - 130.81, 263.74
-# C4 - 261.64, 131.87
 
-if __name__ == "__main__":
-    signal, fs = sf.read("lab_2/a_C4_ugp44.wav")
-
-    # transform to mono
+def find_f0_autocorelation(signal, fs):
+    # transformation to mono
     if signal.ndim == 2:
         signal = signal[:, 0]
 
-    signal = signal[5000:6000]
+    signal = signal[:1000]
 
-    print(signal.shape, fs)
+    # autocorelation
     corr = correlate(signal, signal, mode='full')
     corr = corr[corr.size//2::]
-    peaks, _ = find_peaks(corr, height=0)
 
-    time = corr.shape[0]
+    # finding peaks
+    peaks, peaks_properties = find_peaks(corr, height=0)
+    peak = peaks[np.argmax(peaks_properties['peak_heights'])]
+    f0 = fs/peak
+    print(f0)
+
+    return f0, corr, peaks
+
+
+if __name__ == "__main__":
+    signal, fs = sf.read("lab_2/a_C3_ep44.wav")
+
+    f0, corr, peaks = find_f0_autocorelation(signal, fs)
+
     fig, (ax_orig, ax_corr) = plt.subplots(2, 1)
     ax_orig.plot(signal)
     ax_orig.set_title('Original signal')
@@ -34,6 +41,3 @@ if __name__ == "__main__":
     ax_corr.set_title('Correlation')
     fig.tight_layout()
     plt.show()
-    f0 = fs/peaks[0]
-    print(f0)
-
