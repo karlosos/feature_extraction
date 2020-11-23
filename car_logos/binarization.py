09ef8e69d0b91474c6072cdf8295146d220248e8
Karol Dzialowski <karlososhd@gmail.com>
2020-11-23 18:32:55 +0100

    :construction: segmentation single file

--- car_logos/binarization.py
+++ car_logos/binarization.py
@@ -3,6 +3,9 @@ from skimage.color import rgb2gray, rgba2rgb
 from skimage import img_as_ubyte
 from skimage import io
 from skimage.filters import threshold_otsu
+from skimage.feature import canny
+from scipy import ndimage as ndi
+import numpy as np
 
 folders = ["01_bmw", "01_chevrolet", "02_kia", "03_mitsubishi", "04_opel",
            "05_peugeout", "06_skoda", "07_subaru", "07_honda", "08_tesla",
@@ -13,12 +16,14 @@ for name in folders:
         filename = "./dataset/" + name + "/" + str(i) + ".jpg"
         img = io.imread(filename)
         img2 = rgb2gray(img)
-        thresh = threshold_otsu(img2)
-        binary = img2 > thresh
-        binary = img_as_ubyte(binary)
+        # thresh = threshold_otsu(img2)
+        # binary = img2 > thresh
+        # binary = img_as_ubyte(binary)
+        edges = canny(img2)
+        binary = ndi.binary_fill_holes(edges, structure=np.ones((20, 20)))
 
         # save binarized
         if not os.path.exists("./dataset/binarized/" + name):
             os.makedirs("./dataset/binarized/" + name)
-        path_img_target = os.path.join("./dataset/binarized/" + name + "/" + str(i) + ".jpg")
+        path_img_target = os.path.join("./dataset/binarized/" + name + "/" + str(i) + ".png")
         io.imsave(path_img_target, binary)
