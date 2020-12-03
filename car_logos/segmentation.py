3eb82aa013b7aa3f7422236b5a867f263f17e1b3
Karol Dzialowski <karlososhd@gmail.com>
2020-12-03 18:03:14 +0100

    :recycle: refactoring with black

--- car_logos/segmentation.py
+++ car_logos/segmentation.py
@@ -3,25 +3,36 @@ import numpy as np
 import os
 from pathlib import Path
 
-folders = ["01_bmw", "02_kia", "03_mitsubishi", "04_volvo",
-           "05_peugeout", "06_honda", "07_subaru", "08_tesla",
-           "09_renault", "10_toyota"]
+folders = [
+    "01_bmw",
+    "02_kia",
+    "03_mitsubishi",
+    "04_volvo",
+    "05_peugeout",
+    "06_honda",
+    "07_subaru",
+    "08_tesla",
+    "09_renault",
+    "10_toyota",
+]
 
 for name in folders:
-    files = os.listdir('./dataset/'+name)
+    files = os.listdir("./dataset/" + name)
     for f in files:
         filename = "./dataset/" + name + "/" + f
         im_in = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
-        im_in = cv2.copyMakeBorder(im_in, 20, 20, 20, 20,
-                                   cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        im_in = cv2.copyMakeBorder(
+            im_in, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[255, 255, 255]
+        )
 
         # Threshold.
         # Set values equal to or above 220 to 0.
         # Set values below 220 to 255.
         # th, im_th = cv2.threshold(im_in, 220, 255, cv2.THRESH_BINARY_INV)
         im_th = cv2.adaptiveThreshold(
-            im_in, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+            im_in, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+        )
 
         # im_th = cv2.Canny(im_in, 100, 200)
 
@@ -31,7 +42,7 @@ for name in folders:
         # Mask used to flood filling.
         # Notice the size needs to be 2 pixels than the image.
         h, w = im_th.shape[:2]
-        mask = np.zeros((h+2, w+2), np.uint8)
+        mask = np.zeros((h + 2, w + 2), np.uint8)
 
         # Floodfill from point (0, 0)
         cv2.floodFill(im_floodfill, mask, (0, 0), 255)
@@ -53,8 +64,7 @@ for name in folders:
         # save binarized
         if not os.path.exists("./dataset/binarized/" + name):
             os.makedirs("./dataset/binarized/" + name)
-        path_img_target = os.path.join(
-            "./dataset/binarized/" + name + "/" + f)
-        path_img_target = str(Path(path_img_target).with_suffix('.png'))
+        path_img_target = os.path.join("./dataset/binarized/" + name + "/" + f)
+        path_img_target = str(Path(path_img_target).with_suffix(".png"))
         print(path_img_target)
         cv2.imwrite(path_img_target, im_out)
--- car_logos/simple_shape_descriptors.py
+++ car_logos/simple_shape_descriptors.py
@@ -4,9 +4,18 @@ import os
 from sklearn.model_selection import train_test_split
 import shape_classifier as sc
 
-folders = ["01_bmw", "02_kia", "03_mitsubishi", "04_volvo",
-           "05_peugeout", "06_honda", "07_subaru", "08_tesla",
-           "09_renault", "10_toyota"]
+folders = [
+    "01_bmw",
+    "02_kia",
+    "03_mitsubishi",
+    "04_volvo",
+    "05_peugeout",
+    "06_honda",
+    "07_subaru",
+    "08_tesla",
+    "09_renault",
+    "10_toyota",
+]
 
 
 def prepare_dataset():
@@ -15,12 +24,12 @@ def prepare_dataset():
     X_test = []
     y_test = []
     for name in folders:
-        files = os.listdir('./dataset/binarized/' + name)
+        files = os.listdir("./dataset/binarized/" + name)
         files = [f"./dataset/binarized/{name}/{file}" for file in files]
         y = [name for _ in range(len(files))]
-        X_train_folder, X_test_folder, y_train_folder, y_test_folder = train_test_split(files, y,
-                                                                                        test_size=len(files) - 2,
-                                                                                        random_state=1337)
+        X_train_folder, X_test_folder, y_train_folder, y_test_folder = train_test_split(
+            files, y, test_size=len(files) - 2, random_state=1337
+        )
         X_train += X_train_folder
         y_train += y_train_folder
         X_test += X_test_folder
@@ -38,7 +47,9 @@ def area2(im):
 
 def object_contour(im):
     ret, thresh = cv2.threshold(im, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
     return contours[1]
 
 
@@ -87,9 +98,11 @@ def eccentricity(contour):
 
 def draw_countours(X_train):
     for i in range(len(X_train)):
-        im = cv2.imread(X_train[i], cv2.IMREAD_GRAYSCALE).astype('uint8')
+        im = cv2.imread(X_train[i], cv2.IMREAD_GRAYSCALE).astype("uint8")
         ret, thresh = cv2.threshold(im, 127, 255, 0)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(
+            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
         im = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
         img = cv2.drawContours(im, contours, 1, (255, 0, 0), 3)
         print(contours[1])
@@ -117,5 +130,5 @@ def main():
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
