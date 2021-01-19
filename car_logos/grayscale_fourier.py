66ee38a7f5078cb2285f85c5a7f6d58aa3d7e68c
Karol Dzialowski <karlososhd@gmail.com>
2021-01-19 14:26:30 +0100

    Add moment in grayscale fourier desc

--- car_logos/grayscale_fourier.py
+++ car_logos/grayscale_fourier.py
@@ -6,8 +6,13 @@ def grayscale_fourier_desc(filename, size):
     img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
     # Przekształcenie do skali szarości i do współrzędnych biegunowych
     max_radius = np.sqrt(((img.shape[0] / 2.0) ** 2.0) + ((img.shape[1] / 2.0) ** 2.0))
+    moment = cv2.moments(img)
+    x = int(moment["m10"] / moment["m00"])
+    y = int(moment["m01"] / moment["m00"])
+    centroid = (x, y)
+    centroid = (img.shape[0] / 2, img.shape[1] / 2)
     polar_image = cv2.linearPolar(
-        img, (img.shape[0] / 2, img.shape[1] / 2), max_radius, cv2.WARP_FILL_OUTLIERS
+        img, centroid, max_radius, cv2.WARP_FILL_OUTLIERS
     )
     polar_image = polar_image.astype(np.uint8)
     # polar_image = np.rot90(polar_image)
